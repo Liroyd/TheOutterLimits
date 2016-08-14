5ce6ba54af32cfa0ba7c7ae829a8d7ac719ea2e0
Liroyd <alex_liroyd@yahoo.com>
2016-08-15 01:53:18 +0300

    Add script to move object (mouse, mouse/touch). Touch should be tested. Add test planet, movable object for scene 2. Planet Details

---
+++ Assets/Scripts/Planet View/TapToMove.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TapToMove : MonoBehaviour {
+    private float dist;
+    private bool dragging = false;
+    private Vector3 offset;
+    private Transform toDrag;
+
+    void Update() {
+        UpdateMouse();
+        //UpdateTouch();
+    }
+
+
+    void UpdateTouch() {
+        Vector3 v3;
+
+        if (Input.touchCount != 1) {
+            dragging = false;
+            return;
+        }
+
+        Touch touch = Input.touches[0];
+        Vector3 pos = touch.position;
+
+        if (touch.phase == TouchPhase.Began) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(pos);
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("Here");
+                toDrag = hit.transform;
+                dist = hit.transform.position.z - Camera.main.transform.position.z;
+                v3 = new Vector3(pos.x, pos.y, dist);
+                v3 = Camera.main.ScreenToWorldPoint(v3);
+                offset = toDrag.position - v3;
+                dragging = true;
+            }
+        }
+        if (dragging && touch.phase == TouchPhase.Moved) {
+            v3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
+            v3 = Camera.main.ScreenToWorldPoint(v3);
+            toDrag.position = v3 + offset;
+        }
+        if (dragging && (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)) {
+            dragging = false;
+        }
+    }
+
+    void UpdateMouse() {
+        Vector3 v3;
+
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit  hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                toDrag = hit.transform;
+                dist = hit.transform.position.z - Camera.main.transform.position.z;
+                v3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
+                v3 = Camera.main.ScreenToWorldPoint(v3);
+                offset = toDrag.position - v3;
+                dragging = true;
+            }
+        }
+        if (Input.GetMouseButton(0) && dragging) {
+            v3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
+            v3 = Camera.main.ScreenToWorldPoint(v3);
+            toDrag.position = v3 + offset;
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            dragging = false;
+        }
+    }
+}
