915f3ad27424b01ec455318033dc905ee59b04e0
Liroyd <alex_liroyd@yahoo.com>
2016-08-19 02:00:28 +0300

    Refactor TapToMove script. Add DragDrop on planet functionality

--- Assets/Scripts/Planet View/TapToMove.cs
+++ Assets/Scripts/Planet View/TapToMove.cs
@@ -5,12 +5,14 @@ public class TapToMove : MonoBehaviour {
     private bool dragging = false;
     private Vector3 offset;
     private Transform toDrag;
+    private readonly float INITIAL_POZITION_Z = -450f;
+    private readonly int LAYER_PLANET = 1 << 8;
 
     void Update() {
         #if UNITY_EDITOR
-            UpdateMouse();
+        UpdateMouse();
         #else
-            UpdateTouch();
+        UpdateTouch();
         #endif
     }
 
@@ -47,45 +49,39 @@ public class TapToMove : MonoBehaviour {
         }
     }
 
-     private void onTouchBegin(Vector3 pos) {
-         RaycastHit hit;
-         Ray ray = Camera.main.ScreenPointToRay(pos);
-         if (Physics.Raycast(ray, out hit) && (hit.collider.tag == "Draggable")) {
-             toDrag = hit.transform;
-             dist = hit.transform.position.z - Camera.main.transform.position.z;
-             Vector3 v3 = new Vector3(pos.x, pos.y, dist);
-             v3 = Camera.main.ScreenToWorldPoint(v3);
-             offset = toDrag.position - v3;
-             dragging = true;
-         }
-     }
+    private void onTouchBegin(Vector3 pos) {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(pos);
+        if (Physics.Raycast(ray, out hit) && (hit.collider.tag == "Draggable")) {
+            toDrag = hit.transform;
+            dist = INITIAL_POZITION_Z - Camera.main.transform.position.z;
+            Vector3 v3 = new Vector3(pos.x, pos.y, dist);
+            v3 = Camera.main.ScreenToWorldPoint(v3);
+            offset = new Vector3(toDrag.position.x, toDrag.position.y, INITIAL_POZITION_Z) - v3;
+            dragging = true;
+        }
+    }
 
     private void moveObject(Vector3 v3) {
         v3 = Camera.main.ScreenToWorldPoint(v3);
         toDrag.position = v3 + offset;
-        //TODO: refactor it : drag&drop
-        toDrag.position -= new Vector3(0, 0, toDrag.transform.position.z + 382f);
     }
 
     private void onTouchEnd() {
         dragging = false;
-        test();
+        dropObjectOnPlanet();
     }
 
-    void test() {
-
+    private void dropObjectOnPlanet() {
         RaycastHit  hitCurrentObject;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out hitCurrentObject) && (hitCurrentObject.collider.tag == "Draggable")) {
             RaycastHit  hitPlanetObject;
-            if (Physics.Raycast (ray, out hitPlanetObject, 10000f, 1 << 8)) {
-                hitCurrentObject.transform.position -= new Vector3 (0, 0, hitCurrentObject.transform.position.z
-                + hitPlanetObject.transform.position.z + hitPlanetObject.transform.position.magnitude + 350);
-                //TODO: refactor it : drag&drop
+            var screenPositionOfCurrentObject = Camera.main.WorldToScreenPoint(hitCurrentObject.transform.position);
+            ray = Camera.main.ScreenPointToRay(screenPositionOfCurrentObject);
+            if (Physics.Raycast(ray, out hitPlanetObject, 10000f, LAYER_PLANET)) {
+                hitCurrentObject.transform.position = hitPlanetObject.point;
             }
         }
-
-
-
     }
 }
